85fddbf12701ec61d3bc5455adf1efee9f02b4ed
Carissa Gimenez <41100527+gimenez-iza@users.noreply.github.com>
2024-02-06 18:28:56 +0800

    combining strings

--- strings/app.js
+++ strings/app.js
@@ -3,3 +3,12 @@
 let firstName = 'Iza';
 let lastName = 'Gimenez-Bon';
 let role = 'developer';
+
+// Below role, create a new variable named msg that combines the firstName lastName and role variables to create a string like "Carlos Salgado: developer".
+
+
+
+//Finally, convert the string stored in role to uppercase letters. The final msg string should look similar to this: "Carlos Salgado: DEVELOPER".
+
+let msg = firstName + ' ' + lastName + ': ' + role.toUpperCase();
+console.log(msg);
