ba3442b07a756aacacb6febf7e239cc0d94fc0a3
Carissa Gimenez <41100527+gimenez-iza@users.noreply.github.com>
2024-03-05 17:15:07 +0800

    storing numbers and variables

---
+++ working_with_numbers/app.js
@@ -0,0 +1,7 @@
+//Create a variable named age and assign it your age.
+
+let age = 31;
+
+//Add a second variable named price and assign it a floating point value (a number with a decimal).
+
+let price = 22.75;
