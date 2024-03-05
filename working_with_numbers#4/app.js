9c9eb1841b151717d1bb0699cc258700408bdc24
Carissa Gimenez <41100527+gimenez-iza@users.noreply.github.com>
2024-03-05 17:54:47 +0800

    using math methods code challenge

---
+++ working_with_numbers#4/app.js
@@ -0,0 +1,12 @@
+/*For this Code Challenge you need to use JavaScript's Math methods. Declare a new variable named tempRounded. 
+Assign it the temperature variable rounded to the nearest integer.
+*/
+
+let tempRounded = Math.round(temperature);
+
+/*Next, declare a new variable named tempRoundDown. 
+Assign it the temperature variable rounded downward to the nearest integer. 
+Check the Mozilla Developer Network to find the proper Math method (hint: down is toward the "floor".)
+*/
+
+let tempRoundDown = Math.floor(temperature);
